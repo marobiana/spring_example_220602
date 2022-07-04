4281ba838b768ba43e3bc368ae16bb69477211ac
DESKTOP-G4N1KD8\user <aaaa@gmail.com>
2022-07-04 18:37:44 +0900

    5강 fn JSTL 라이브러리 예제

--- src/main/java/com/example/lesson05/Lesson05Controller.java
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -57,6 +57,11 @@ public class Lesson05Controller {
 		model.addAttribute("today", today);
 		return "lesson05/ex03";
 	}
+	
+	@RequestMapping("/lesson05/ex04")
+	public String ex04() {
+		return "lesson05/ex04";
+	}
 }
 
 
